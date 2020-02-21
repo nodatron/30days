ac9a092e004bee6f0fb7aea91fc0756898eceb43
Niall O'Donnell <niallod1996@gmail.com>
2020-02-21 12:16:33 +0000

    day one and two in python

---
+++ 2-data-types/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+i2 = None
+d2 = None
+s2 = None
+# Read and save an integer, double, and String to your variables.
+i2 = int(input())
+d2 = float(input())
+s2 = str(raw_input())
+# Print the sum of both integer variables on a new line.
+print i2 + i
+# Print the sum of the double variables on a new line.
+print d2 + d
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + s2
